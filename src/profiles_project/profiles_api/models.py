d52ece32286a7e70542b850aabda8f5e544745ba
MervynOng <mervynongxh@gmail.com>
2020-06-08 06:26:32 +0800

    added custom user profile mode, a manager and migrations for database

--- src/profiles_project/profiles_api/models.py
+++ src/profiles_project/profiles_api/models.py
@@ -1,3 +1,83 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser #import it so we can later substitue it woth our very own custom model
+from django.contrib.auth.models import PermissionsMixin
+from django.contrib.auth.models import BaseUserManager
 
-# Create your models here.
+class UserProfileManager(BaseUserManager):
+    """Helps Django work with our custom user model"""
+
+    def create_user(self, email, name, password=None):
+            """Creates a new user profile object"""
+
+            #when no email or invalid
+            if not email:
+                raise ValueError('Users must have an email address.')
+
+            #normalize converts all into lowercase
+            email = self.normalize_email(email)
+            user = self.model(email=email, name=name)
+
+            #it will encrypt password
+            user.set_password(password)
+            user.save(using=self._db)
+
+            return user
+
+    #admin
+    def create_superuser(self, email, name, password):
+        """Creates and saves a new superuser with given details"""
+
+        user = self.create_user(email, name, password)
+
+        user.is_superuser = True
+        user.is_staff = True
+
+        user.save(using=self._db)
+
+        return user
+
+
+class UserProfile(AbstractBaseUser, PermissionsMixin):
+    """Represent a "user profile" inside our system"""
+
+    email = models.EmailField(max_length=255, unique=True) #unique means one and only one
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True) #needed for substition later
+    is_staff = models.BooleanField(default=False) #needed for substition later
+
+    #object manager to manage user profile *needed for substition later
+    objects = UserProfileManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['name']
+
+    #will be used later on with the django admin
+    def get_full_name(self):                        #possible to make this into a get_first_name and get_last_name
+        """Used to get a users full name"""
+
+        return self.name
+
+    def get_short_name(self):                       #possible to make this into  a get_first_name or get_last_name
+        """Used to get a users short name"""
+
+        return self.name
+
+    def __str__(self):
+        """Django uses this when it needs to convert the object to a string"""
+
+        return self.email
+
+class ProfileFeedItem(models.Model):
+    """Profile status update"""
+
+    # use foreign key to get to another model
+    user_profile = models.ForeignKey('UserProfile', on_delete=models.CASCADE)#if user profile is deleted, then all profile feed will also be deleted
+    # create status
+    status_text = models.CharField(max_length=255)
+    # store date status is created
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        """Return the model as a string."""
+
+        return self.status_text
