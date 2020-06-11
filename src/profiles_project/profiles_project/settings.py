4df6a9affc77515b15e23620058799c51df7067c
MervynOng <mervynongxh@gmail.com>
2020-06-11 18:29:59 +0800

    adding static files to fix UI

--- src/profiles_project/profiles_project/settings.py
+++ src/profiles_project/profiles_project/settings.py
@@ -119,7 +119,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
-
 STATIC_URL = '/static/'
+
+STATIC_ROOT = (
+    os.path.join(BASE_DIR, 'static')
+)
 
 AUTH_USER_MODEL = 'profiles_api.UserProfile'
